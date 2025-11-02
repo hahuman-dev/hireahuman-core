0ab21145b4310bb03e74bd99f2818e465190fd3c
Demus <dean@medicaltracker.co.uk>
2025-11-02 12:48:48 +0000

    update cors

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ app.use(
       "http://localhost:3000",
       "http://127.0.0.1:3000",
       "https://app.hireahuman.com",
+      "https://app-dev.hireahuman.com",
       "https://hireahuman.com",
     ],
     credentials: true,
